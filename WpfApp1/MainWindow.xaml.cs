385fa5a0f38b0ed63898e602b63d8b2cf623a57a
Efrain Rodriguez Vera <efrain7805@users.noreply.github.com>
2020-05-03 21:09:11 -0500

    mensaje de hola

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace WpfApp1
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Hola");
+        }
     }
 }
